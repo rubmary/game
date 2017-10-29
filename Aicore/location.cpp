3c262efc7ded1359cc4d5f92496c0d577baf0c75
rubmary <rubmaryrojas@gmail.com>
2017-10-29 19:09:26 -0400

    Clean code

--- Aicore/location.cpp
+++ Aicore/location.cpp
@@ -25,10 +25,6 @@ Vector3<double> Location::get_orientation_as_vector() {
 }
 
 
-// Kinematic's function
-void Kinematic::integrate(double t) {
-    SIMPLE_INTEGRATION(t, velocity, rotation);
-}
 
 void Kinematic::integrate(const SteeringOutput& steer, double t)
 {
@@ -39,48 +35,4 @@ void Kinematic::integrate(const SteeringOutput& steer, double t)
         velocity *= max_speed;
     }
     rotation += steer.angular*t;
-}
-
-
-void Kinematic::integrate(const SteeringOutput& steer, double drag, double t) {
-    SIMPLE_INTEGRATION(t, velocity, rotation);
-
-    drag = pow(drag, t);
-    velocity *= drag;
-    rotation *= drag;
-    velocity += steer.linear*t;
-    rotation += steer.angular*t;
-}
-
-void Kinematic::integrate(const SteeringOutput& steer, 
-                          const SteeringOutput& drag, 
-                          double t) {
-    SIMPLE_INTEGRATION(t, velocity, rotation);
-    velocity.x *= pow(drag.linear.x, t);
-    velocity.y *= pow(drag.linear.y, t);
-    velocity.z *= pow(drag.linear.z, t);
-    rotation   *= pow(drag.angular,  t);
-
-    velocity += steer.linear*t;
-    rotation += steer.angular*t;
-}
-void Kinematic::operator += (const Kinematic& other) {
-    position += other.position;
-    velocity += other.velocity;
-    rotation += other.rotation;
-    orientation += other.orientation; 
-}
-
-void Kinematic::operator -= (const Kinematic& other) {
-    position -= other.position;
-    velocity -= other.velocity;
-    rotation -= other.rotation;
-    orientation -= other.orientation; 
-}
-
-void Kinematic::operator *= (double f) {
-    position *= f;
-    velocity *= f;
-    rotation *= f;
-    orientation *= f;
 }
--- Engine.h
+++ Engine.h
@@ -37,30 +37,6 @@ public:
     Engine(int width, int height, int margin);
 };
 
-class EngineKinematic : public Engine {  
-    // Kinematics
-    KinematicSeek   k_seek;
-    KinematicFlee   k_flee;
-    KinematicArrive k_arrive;
-    KinematicWander k_wander;
-
-public:
-    EngineKinematic(int width, int height, int margin);
-    // Kinematic's behaviors
-    void kinematic_start(double max_speed);
-
-    void kinematic_seek(double time);
-
-    void kinematic_flee(double time);
-
-    void kinematic_arrive(double time);
-
-    void kinematic_wander(double time);
-
-    void start();
-
-};
-
 class EngineSteering : public Engine {  
     // Steerings
 public:
@@ -75,26 +51,6 @@ public:
     EngineSteering(int width, int height, int margin);
     // Kinematic's behaviors
     void steering_start(double max_speed);
-
-    void steering_seek(double time);
-
-    void steering_flee(double time);
-
-    void steering_pursue(double time);
-
-    void steering_arrive(double time);
-
-    void steering_wander(double time);
-
-    void start_seek();
-
-    void start_flee();
-
-    void start_pursue();
-
-    void start_arrive();
-
-    void start_wander();
 };
 
 class EngineFollowPath : public Engine  {
